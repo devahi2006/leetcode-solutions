75c3988b051f1fa54efdc6898eee9539b6c25488
devahi2006 <devahidevahi406@gmail.com>
2026-03-12 22:48:26 +0530

    Added ReorderList solution

---
+++ LinkedList/ReorderList.java
@@ -0,0 +1,36 @@
+//medium
+//prbl.no:143
+class Solution {
+    public void reorderList(ListNode head) {
+        
+        if(head == null) return;
+        
+        ListNode slow = head, fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode prev = null, curr = slow.next;
+        slow.next = null;
+        
+        while(curr != null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        ListNode first = head, second = prev;
+        
+        while(second != null){
+            ListNode temp1 = first.next;
+            ListNode temp2 = second.next;
+            
+            first.next = second;
+            second.next = temp1;
+            
+            first = temp1;
+            second = temp2;
+        }
+    }
+}
