75ae22e0fb586d3d47ab26d0a0e3f8840ab0099e
devahi2006 <devahidevahi406@gmail.com>
2026-03-15 11:53:17 +0530

    Added MergeKSortedList solution

---
+++ LinkedList/MergeKSortedList.java
@@ -0,0 +1,21 @@
+//hard
+//prbl.no=23
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer>minHeap=new PriorityQueue<>();
+        for(ListNode list:lists){
+            while(list!=null){
+                minHeap.add(list.val);
+                list=list.next;
+            }
+        }
+            ListNode dummy=new ListNode(1);
+            ListNode merge=dummy;
+            while(!minHeap.isEmpty()){
+                merge.next=new ListNode(minHeap.remove());
+                merge=merge.next;
+            }
+            return dummy.next;
+
+        }
+    }
